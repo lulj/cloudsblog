4bd6a9841a9548ec2d171eeebb08f4068912ca9f
ben <cloudsben@gmail.com>
2012-01-17 15:46:27 +0800

    config

--- app/config/database.php
+++ app/config/database.php
@@ -48,12 +48,12 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['hostname'] = '192.168.4.44';
+$db['default']['username'] = 'blog';
+$db['default']['password'] = 'benben';
+$db['default']['database'] = 'blog';
 $db['default']['dbdriver'] = 'mysql';
-$db['default']['dbprefix'] = '';
+$db['default']['dbprefix'] = 'mz_';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
